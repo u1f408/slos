07aba36e5e5845fe629ae4d45ae9da271ed1567d
Iris System <iris@iris.ac.nz>
2021-08-26 15:50:36 +1200

    core: remove Shutdown, allow bubbling fs MountError

--- src/errors.rs
+++ src/errors.rs
@@ -2,9 +2,15 @@ use displaydoc::Display as DisplayDoc;
 
 #[derive(DisplayDoc, Debug)]
 pub enum KernelError {
-	/// Kernel shutdown
-	Shutdown,
+	/// Bubbled filesystem mount error
+	MountError(slos_filesystem::MountError),
 
 	/// Unknown error
 	Unknown,
+}
+
+impl From<slos_filesystem::MountError> for KernelError {
+	fn from(e: slos_filesystem::MountError) -> KernelError {
+		KernelError::MountError(e)
+	}
 }
